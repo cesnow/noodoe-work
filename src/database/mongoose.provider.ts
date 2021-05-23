abd863b91cb0bb71387e44423af90c45a9d1c12c
evan <evan@TING-YUdeMac-mini.local>
2021-05-24 03:51:15 +0800

    removed database connection information

--- src/database/mongoose.provider.ts
+++ src/database/mongoose.provider.ts
@@ -1,8 +1,8 @@
 import {MongooseModule, MongooseModuleOptions} from '@nestjs/mongoose';
 
 const config: MongooseModuleOptions = {
-  user: 'liquid',
-  pass: 'C4p5nmn9tTvB6M2m',
+  user: '[your mongodb username]',
+  pass: '[your mongodb password]',
   connectTimeoutMS: 1000,
   socketTimeoutMS: 0,
   poolSize: 15,
@@ -15,6 +15,6 @@ const config: MongooseModuleOptions = {
 };
 
 export const MongooseProvider = MongooseModule.forRoot(
-  'mongodb+srv://liquid.t0knb.mongodb.net/noodoe-work',
+  '[your mongodb uri with database name]', // like mongodb+srv://liquid.t0knb.mongodb.net/noodoe-work
   config
 );
